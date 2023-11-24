d20fd50207de6d0f690b5547517bfb77a7ba3f27
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-11-24 12:28:52 -0600

    Eliminando elementos del array

--- src/01-array.js
+++ src/01-array.js
@@ -13,10 +13,37 @@ class MyArray {
         this.length++;
         return this.data;
     }
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+    delete(index) {
+        const item = this.data[index];
+        this.shiftIndex(index);
+        return item;
+    }
+    shiftIndex(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
 }
 
 const myArray = new MyArray();
 
 myArray.push("Christopher");
 myArray.push("Hector");
-console.log(myArray.get(1));
+myArray.push("Francisco");
+console.log("array", myArray);
+console.log("POP", myArray.pop());
+console.log("arrayBorrado", myArray);
+myArray.push("Antonio");
+myArray.push("Federico");
+myArray.push("Juan");
+console.log("nuevoArray", myArray);
+myArray.delete(3);
+console.log("arrayNew", myArray);
